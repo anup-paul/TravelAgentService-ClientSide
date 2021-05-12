2326419e74d333c9cb322a3f5cf89295a60ed1c3
anup-paul <anuppaul1845@gmail.com>
2021-05-12 16:06:12 +0600

    check

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -12,6 +12,7 @@ const Header = () => {
                             <h3><b>We provide the best experience for you to explore the nature</b></h3>
                             <p className="text-secondary my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error soluta eos, maiores illo odio mollitia accusamus aliquam deserunt assumenda suscipit.</p>
                             <button className="btn btn-primary" > Learn More </button>
+                            <h2>Check</h2>
                         </div>
                     </div>
                     <div className="col-md-6">
--- src/Components/Login/firebaseConfig.js
+++ src/Components/Login/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBDjavyURM8MdtRPes1r-VJLDWWGQizyQw",
-    authDomain: "travel-service-ded51.firebaseapp.com",
-    projectId: "travel-service-ded51",
-    storageBucket: "travel-service-ded51.appspot.com",
-    messagingSenderId: "698712992939",
-    appId: "1:698712992939:web:ed775eb705ece0034d9bee"
-  };
+  apiKey: "AIzaSyCf5SmioX4YLY2xJPELIFFfUNEUIUZ0L_o",
+  authDomain: "travel-agent-service.firebaseapp.com",
+  projectId: "travel-agent-service",
+  storageBucket: "travel-agent-service.appspot.com",
+  messagingSenderId: "825804316427",
+  appId: "1:825804316427:web:2c281169974e9e24760c0e"
+};
 
   export default firebaseConfig ;
